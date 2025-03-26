ca4c08955b347a494673118a9bda4c37252e7e64
Guilherme Higino <guilhermehiginoo@gmail.com>
2025-03-26 14:44:37 -0300

    Saving questions for later

---
+++ unidade-I/lista-de-exercicios-a01/questao1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int combinations(int n, int k){
+    if()
+    combinations();
+}
+
+int main() {
+    int n, k;
+    scanf("Insert value for n and k: %d %d", &n, &k);
+    combinations(n, k);
+}
